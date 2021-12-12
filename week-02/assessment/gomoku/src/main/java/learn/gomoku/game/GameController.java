35882a54dc0e92b301c770d7cb866ece4a258033
Casey Schultz <c.b.schultz54@gmail.com>
2021-12-12 14:35:39 -0600

    early version

--- week-02/assessment/gomoku/src/main/java/learn/gomoku/game/GameController.java
+++ week-02/assessment/gomoku/src/main/java/learn/gomoku/game/GameController.java
@@ -8,11 +8,87 @@ import learn.gomoku.players.RandomPlayer;
 
 
 public class GameController {
-
     public void run() {
 
         Scanner console = new Scanner(System.in);
 
+        Gomoku game = setUp(console);
+
+        while (!game.isOver()){
+            System.out.println("It's your turn " + game.getCurrent().getName());
+            Stone stone = game.getCurrent().generateMove(game.getStones());
+            Result result = game.place(stone);
+            if (!result.isSuccess()){
+                System.out.println("Err: " + result.getMessage());
+            }
+        }
+        if (game.getWinner() == null){
+            System.out.println("It's a tie!");
+        }else{
+            System.out.println("The winner is: " + game.getWinner());
+        }
+
+//        System.out.println("readInt(\"Submit only ints or die\" , 15, 15) = " + readInt("Submit only ints or die", 15, 15));
+
+
+
+        playAgain("Do you want to play again? [y/n] ");
+
+
+    }
+
+    public static String readRequiredString(String prompt) {
+        Scanner console = new Scanner(System.in);
+        String result;
+        do {
+            System.out.print(prompt);
+            result = console.nextLine();
+        } while (result.length() == 0);
+        return result;
+    }
+
+    static final char[] digits = {
+            '0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9'
+    };
+
+    private static boolean isNumber(char theChar) {
+        for (int j = 0; (j <= digits.length - 1); j++) {
+            if (theChar == digits[j]) {
+                return true;
+            }
+        }
+        System.out.println("That's not a valid input");
+        return false;
+    }
+
+    private static boolean validateTheInputIsANumber(String theInput) {
+        boolean validateNextChar = true;
+
+        for (int i = 0; (i <= theInput.length() - 1) && validateNextChar; i++) {
+            char theChar = theInput.charAt(i);
+            validateNextChar = isNumber(theChar);
+        }
+        return validateNextChar;
+    }
+
+    public static int readInt(String message, int min, int max) {
+        String theInput;
+        boolean valid = false;
+        do {
+            System.out.print("Enter the coordinates of your move: ");
+            theInput = readRequiredString(message);
+            if (theInput.length() > 0) {
+                valid = validateTheInputIsANumber(theInput);
+            }
+        }
+        while (!valid);
+
+
+        return Integer.parseInt(theInput);
+    }
+
+    private Gomoku setUp(Scanner console) {
         System.out.println("Welcome to Gomoku");
         System.out.println("=".repeat(18) + "\n");
 
